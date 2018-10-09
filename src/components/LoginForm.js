b29ef86f9c7867d0600b5f4e22a8a8be169b0fff
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-10-08 21:03:44 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,33 +1,68 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      password: '',
+      username: '',
+    };
   }
+  handleInputChange = event => {
+  const { name, value } = event.target;
+  console.log(name);
+  this.setState({
+    [name]: value,
+  });
+};
+  handleChange = event => {
+   this.setState({
+     password: event.target.value,
+     username: event.target.value,
+   });
+ }
+ handleFormSubmit = event => {
+     event.preventDefault();
+     const { username, password } = this.state;
+
+     if (!username || !password) {
+       return;
+     }
+
+     this.props.onSubmit({ username, password });
+   };
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleFormSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text"
+            value={this.state.username}
+
+            name="username" onChange={this.handleInputChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" name="password"
+            value={this.state.password} onChange={this.handleInputChange} />
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button type="submit" onClick={this.handleFormSubmit}>Log in</button>
         </div>
       </form>
     );
   }
 }
+
+LoginForm.propTypes = {
+  onSubmit: PropTypes.func,
+};
 
 export default LoginForm;
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,25 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: '',
+    };
   }
+  handleChange = event => {
+   this.setState({
+     value: event.target.value,
+   });
+ }
+ counterChange = event => {
+   return 140 - this.state.value.length
+ }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <p>{140 - this.state.value.length}</p>
       </div>
     );
   }
